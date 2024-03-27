864500d13ced28418a52734d5621656499518d5b
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-03-27 17:03:46 +0100

    feat: some changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,25 @@
 const row = document.querySelector(".row");
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 0; i <= 107; i++) {
   const squareElem = document.createElement("div");
   squareElem.classList = "square";
 
   //Console
   if (i % 3 === 0 && i % 5 === 0) {
-    squareElem.innerHTML = ` <h3>BoolBuzz</h3>`;
+    squareElem.innerHTML = ` <h3>Lucaris</h3>`;
     squareElem.classList.add("fizzbuzz");
 
     // debug
     console.log("FizzBuzz");
-
   } else if (i % 3 === 0) {
-    squareElem.innerHTML = ` <h3>Bool</h3>`;
+    squareElem.innerHTML = ` <h3>Loris</h3>`;
     squareElem.classList.add("fizz");
-
 
     // debug
     console.log("Fizz");
-    
   } else if (i % 5 === 0) {
-    squareElem.innerHTML = ` <h3>Buzz</h3>`;
+    squareElem.innerHTML = ` <h3>Luca</h3>`;
     squareElem.classList.add("buzz");
-
 
     // debug
     console.log("Buzz");
